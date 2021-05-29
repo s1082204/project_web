344ccd0cb68c7e5932ed3aea48b5376673d28887
黃重皓 <sokve855@gmail.com>
2021-05-29 23:29:25 +0800

    web of project

---
+++ text_04.js
@@ -0,0 +1,9 @@
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+});
+
+$('#toggle_2').click(function() {
+    $(this).toggleClass('active');
+    $('#Hidden-area').toggleClass('open');
+});
