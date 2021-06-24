69a69dc814f0ed5088dba25b23592199dd5ffa66
黃重皓 <sokve855@gmail.com>
2021-06-25 00:19:41 +0800

    加時間

--- SmartGo.js
+++ SmartGo.js
@@ -44,4 +44,15 @@ function next(){
 function login(){
     
 }
-
+ 
+let newTimer =()=> {
+    let date = new Date();
+    let str = date.toLocaleDateString();
+    str += "  " + date.toLocaleTimeString();
+    let odiv = document.getElementById("time");
+    odiv.innerHTML = str;
+    setTimeout(newTimer, 1000);
+    }    
+    window.onload =()=>{
+    newTimer();
+}
